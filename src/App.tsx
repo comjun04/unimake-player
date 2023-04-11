435a6b9d15cbe4b83f5d032f00202195da11479a
comjun04 <comjun04@gmail.com>
2023-04-12 01:28:41 +0900

    feat: play multiple sound mappings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { FC, useEffect, useState } from "react"
+import { FC, useEffect, useMemo, useState } from "react"
 import { Container } from "react-bootstrap"
 import PackInfo from "@/components/PackInfo"
 import Pad from "@/components/Pad"
 import PackLoadModal from "./components/PackLoadModal"
 import useProcessPack, { IPackData } from "./hooks/useProcessPack"
 
+const initialPadBtnPressCount = Array(8)
+for (let i = 0; i < 8; i++) {
+  initialPadBtnPressCount[i] = Array(8).fill(0)
+}
+
+function arrayDeepCopy(arr: number[][]) {
+  const newArr: number[][] = []
+  arr.forEach((el) => {
+    newArr.push(el.slice())
+  })
+
+  return newArr
+}
+
 const App: FC = () => {
   const [showPackLoadModal, setShowPackLoadModal] = useState(false)
   const [packData, setPackData] = useState<IPackData>()
   const [chain, setChain] = useState(1)
+
+  const [padBtnPressCount, setPadBtnPressCount] = useState<number[][]>(
+    initialPadBtnPressCount
+  )
+
   const { status, processPack, clear } = useProcessPack()
 
   const handleLoadPack = async (file: File) => {
@@ -38,9 +57,20 @@ const App: FC = () => {
 
     const key = `${chain} ${position.x} ${position.y}`
     if (mappings.has(key)) {
-      const mapping = mappings.get(key)![0]
+      const pressedCount = padBtnPressCount[position.x - 1][position.y - 1]
+      const thisBtnMappings = mappings.get(key)!
+
+      console.log(position, pressedCount)
+
+      const mapping = thisBtnMappings[pressedCount]
       const sound = howlers.get(mapping.soundName)
       sound?.play()
+
+      const nextPressedCount =
+        pressedCount + 1 >= thisBtnMappings.length ? 0 : pressedCount + 1
+      const newPadBtnPressCount = arrayDeepCopy(padBtnPressCount)
+      newPadBtnPressCount[position.x - 1][position.y - 1] = nextPressedCount
+      setPadBtnPressCount(newPadBtnPressCount)
 
       const t = Date.now()
       console.log("btn pressed")
@@ -54,6 +84,11 @@ const App: FC = () => {
       clear()
     }
   }, [showPackLoadModal])
+
+  // pack을 load하거나 chain이 바뀔 때 버튼 누른 횟수 초기화
+  useEffect(() => {
+    setPadBtnPressCount(initialPadBtnPressCount)
+  }, [packData, chain])
 
   return (
     <>
