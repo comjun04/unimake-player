b5fd6ec72eb053984916afd357ab96185bd156b0
comjun04 <comjun04@gmail.com>
2023-04-14 01:02:39 +0900

    feat: calibrated timeout

--- src/hooks/useAutoplay.ts
+++ src/hooks/useAutoplay.ts
@@ -1,5 +1,6 @@
 import type { TAutoplayData } from "@/types"
 import { useEffect, useRef, useState } from "react"
+import useCalibratedTimeout from "./useCalibratedTimeout"
 
 const useAutoplay = (data: TAutoplayData[]) => {
   const [idx, setIdx] = useState(0)
@@ -7,6 +8,8 @@ const useAutoplay = (data: TAutoplayData[]) => {
 
   const playingRef = useRef(playing)
   playingRef.current = playing
+
+  const { setCalibratedTimeout } = useCalibratedTimeout()
 
   const current = data[idx]
   const current2 = current?.type === "delay" ? data[idx - 1] : current
@@ -29,7 +32,7 @@ const useAutoplay = (data: TAutoplayData[]) => {
     }
 
     if (current.type === "delay") {
-      const timer = setTimeout(() => {
+      const timer = setCalibratedTimeout(() => {
         if (playingRef.current) {
           setIdx(idx + 1)
         }
---
+++ src/hooks/useCalibratedTimeout.ts
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const useCalibratedTimeout = () => {
+  const [offset, setOffset] = useState(0)
+
+  const timeoutFn = (fn: () => void, time: number) => {
+    const expected = Date.now() + time
+
+    const timerId = window.setTimeout(() => {
+      const dt = Date.now() - expected
+      console.log(dt)
+
+      fn()
+      setOffset(Math.max(0, dt))
+    }, time - offset)
+    return timerId
+  }
+
+  return { setCalibratedTimeout: timeoutFn, offset }
+}
+
+export default useCalibratedTimeout
