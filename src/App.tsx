280f8ebf36ec42eb46f4565c4426dcbc12719225
comjun04 <comjun04@gmail.com>
2024-06-12 03:07:20 +0900

    perf: skip render pack load modal on autoplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { FC, useEffect, useState } from "react"
+import { FC, useState } from "react"
 import { Container } from "react-bootstrap"
 import PackInfo from "@/components/PackInfo"
-import Pad from "@/components/Pad"
 import { IPackData } from "./hooks/useProcessPack"
-import AutoplayControl from "./components/AutoplayControl"
-import { TButtonPosition } from "./types"
-import useAutoplay from "./hooks/useAutoplay"
 import PackLoadWrapper from "./components/PackLoadWrapper"
-import { usePackStore, usePadButtonsStore, usePadStore } from "./store"
+import { usePackStore } from "./store"
 import { useShallow } from "zustand/react/shallow"
+import PlayArea from "./PlayArea"
 
 const App: FC = () => {
   const [showPackLoadModal, setShowPackLoadModal] = useState(false)
@@ -21,105 +18,12 @@ const App: FC = () => {
     }))
   )
 
-  const { chain, setChain } = usePadStore(
-    useShallow((state) => ({
-      chain: state.chain,
-      setChain: state.setChain,
-    }))
-  )
-
-  const { markBtnPressed, markBtnReleased, resetAllBtnPressCount } =
-    usePadButtonsStore(
-      useShallow((state) => ({
-        markBtnPressed: state.press,
-        markBtnReleased: state.release,
-        resetAllBtnPressCount: state.resetAllPressCount,
-      }))
-    )
-
-  const {
-    currentSegment: currentAutoplaySegment,
-    start: startAutoplay,
-    stop: stopAutoplay,
-    reset: resetAutoplay,
-    playing: autoplaying,
-    now: autoplayNowIdx,
-    total: autoplayTotalIdx,
-  } = useAutoplay(packData?.autoplay ?? [])
-
-  const handlePackLoadComplete = (packData: IPackData) => {
-    if (packData) {
-      setPackData(packData)
+  const handlePackLoadComplete = (data: IPackData) => {
+    if (data != null) {
+      setPackData(data)
       setShowPackLoadModal(false)
     }
   }
-
-  const handleBtnPress = (position: TButtonPosition) => {
-    // console.log(position)
-
-    // mc button
-    if (position?.mc != null) {
-      if (position.mc !== chain) {
-        setChain(position.mc)
-      }
-      return
-    }
-
-    markBtnPressed(position.x, position.y, chain)
-  }
-
-  const handleBtnRelease = (position: TButtonPosition) => {
-    if (position.mc != null) return
-
-    markBtnReleased(position.x, position.y)
-  }
-
-  // pack을 load하거나 chain이 바뀔 때 버튼 누른 횟수 초기화
-  useEffect(() => {
-    resetAllBtnPressCount()
-  }, [packData, chain])
-
-  // autoplay segment 처리
-  useEffect(() => {
-    if (!currentAutoplaySegment) return
-
-    switch (currentAutoplaySegment.type) {
-      case "on":
-        handleBtnPress({
-          x: currentAutoplaySegment.x,
-          y: currentAutoplaySegment.y,
-        })
-        break
-
-      case "off":
-        handleBtnRelease({
-          x: currentAutoplaySegment.x,
-          y: currentAutoplaySegment.y,
-        })
-        break
-
-      case "touch": {
-        handleBtnPress({
-          x: currentAutoplaySegment.x,
-          y: currentAutoplaySegment.y,
-        })
-
-        // TODO: clear timer when unmount
-        setTimeout(() => {
-          // currentAutoplaySegment 값이 timeout 전에 바뀌었어도 여기 있는 값은 변하지 않음
-          handleBtnRelease({
-            x: currentAutoplaySegment.x,
-            y: currentAutoplaySegment.y,
-          })
-        }, 150)
-        break
-      }
-
-      case "chain":
-        setChain(currentAutoplaySegment.chain)
-        break
-    }
-  }, [currentAutoplaySegment])
 
   return (
     <>
@@ -128,15 +32,7 @@ const App: FC = () => {
           onLoadPackClick={() => setShowPackLoadModal(true)}
           infoData={packData?.info ?? {}}
         />
-        <AutoplayControl
-          playing={autoplaying}
-          onPlay={startAutoplay}
-          onPause={stopAutoplay}
-          onStop={resetAutoplay}
-          now={autoplayNowIdx}
-          total={autoplayTotalIdx}
-        />
-        <Pad onBtnPress={handleBtnPress} onBtnRelease={handleBtnRelease} />
+        <PlayArea />
       </Container>
       <PackLoadWrapper
         showModal={showPackLoadModal}
---
+++ src/PlayArea.tsx
@@ -0,0 +1,120 @@
+import { FC, useEffect } from "react"
+import AutoplayControl from "./components/AutoplayControl"
+import Pad from "./components/Pad"
+import { usePackStore, usePadButtonsStore, usePadStore } from "./store"
+import { useShallow } from "zustand/react/shallow"
+import useAutoplay from "./hooks/useAutoplay"
+import { TButtonPosition } from "./types"
+
+const PlayArea: FC = () => {
+  const packData = usePackStore((state) => state.packData)
+
+  const { chain, setChain } = usePadStore(
+    useShallow((state) => ({
+      chain: state.chain,
+      setChain: state.setChain,
+    }))
+  )
+
+  const { markBtnPressed, markBtnReleased, resetAllBtnPressCount } =
+    usePadButtonsStore(
+      useShallow((state) => ({
+        markBtnPressed: state.press,
+        markBtnReleased: state.release,
+        resetAllBtnPressCount: state.resetAllPressCount,
+      }))
+    )
+
+  const {
+    currentSegment: currentAutoplaySegment,
+    start: startAutoplay,
+    stop: stopAutoplay,
+    reset: resetAutoplay,
+    playing: autoplaying,
+    now: autoplayNowIdx,
+    total: autoplayTotalIdx,
+  } = useAutoplay(packData?.autoplay ?? [])
+
+  const handleBtnPress = (position: TButtonPosition) => {
+    // console.log(position)
+
+    // mc button
+    if (position?.mc != null) {
+      if (position.mc !== chain) {
+        setChain(position.mc)
+      }
+      return
+    }
+
+    markBtnPressed(position.x, position.y, chain)
+  }
+
+  const handleBtnRelease = (position: TButtonPosition) => {
+    if (position.mc != null) return
+
+    markBtnReleased(position.x, position.y)
+  }
+
+  // pack을 load하거나 chain이 바뀔 때 버튼 누른 횟수 초기화
+  useEffect(() => {
+    resetAllBtnPressCount()
+  }, [packData, chain])
+
+  // autoplay segment 처리
+  useEffect(() => {
+    if (!currentAutoplaySegment) return
+
+    switch (currentAutoplaySegment.type) {
+      case "on":
+        handleBtnPress({
+          x: currentAutoplaySegment.x,
+          y: currentAutoplaySegment.y,
+        })
+        break
+
+      case "off":
+        handleBtnRelease({
+          x: currentAutoplaySegment.x,
+          y: currentAutoplaySegment.y,
+        })
+        break
+
+      case "touch": {
+        handleBtnPress({
+          x: currentAutoplaySegment.x,
+          y: currentAutoplaySegment.y,
+        })
+
+        // TODO: clear timer when unmount
+        setTimeout(() => {
+          // currentAutoplaySegment 값이 timeout 전에 바뀌었어도 여기 있는 값은 변하지 않음
+          handleBtnRelease({
+            x: currentAutoplaySegment.x,
+            y: currentAutoplaySegment.y,
+          })
+        }, 150)
+        break
+      }
+
+      case "chain":
+        setChain(currentAutoplaySegment.chain)
+        break
+    }
+  }, [currentAutoplaySegment])
+
+  return (
+    <>
+      <AutoplayControl
+        playing={autoplaying}
+        onPlay={startAutoplay}
+        onPause={stopAutoplay}
+        onStop={resetAutoplay}
+        now={autoplayNowIdx}
+        total={autoplayTotalIdx}
+      />
+      <Pad onBtnPress={handleBtnPress} onBtnRelease={handleBtnRelease} />
+    </>
+  )
+}
+
+export default PlayArea
